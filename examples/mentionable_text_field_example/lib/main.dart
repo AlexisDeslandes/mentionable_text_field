9d0839619aff86d0920c4eaa42b95541b92a23a8
alexisdeslandes <alexis.deslandes@planisware.com>
2022-10-03 06:57:32 +0200

    Add animation to example

--- examples/mentionable_text_field_example/lib/main.dart
+++ examples/mentionable_text_field_example/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mentionable_text_field/mentionable_text_field.dart';
+import 'package:rxdart/subjects.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +18,23 @@ class MyUser extends Mentionable {
 
   @override
   String buildMention() => '<my-custom-tag>$mentionLabel</my-custom-tag>';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MyUser &&
+          runtimeType == other.runtimeType &&
+          mentionLabel == other.mentionLabel;
+
+  @override
+  int get hashCode => mentionLabel.hashCode;
+}
+
+extension BouncyAnimation on Animation<double> {
+  Animation<double> driveBouncy() => drive(TweenSequence([
+        TweenSequenceItem(tween: Tween(begin: 0.0, end: 1.3), weight: 0.7),
+        TweenSequenceItem(tween: Tween(begin: 1.3, end: 1.0), weight: 0.3)
+      ]));
 }
 
 /// App widget that shows mentionable_text_field example implementation.
@@ -48,57 +67,121 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<Mentionable> _mentionableUsers = [];
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  final BehaviorSubject<List<Mentionable>> _mentionableStreamController =
+      BehaviorSubject.seeded([]);
+  late final AnimationController _animationController =
+      AnimationController(vsync: this);
   late void Function(Mentionable mentionable) _onSelectMention;
+
+  @override
+  void dispose() {
+    _mentionableStreamController.close();
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned(
-              bottom: 0,
-              width: MediaQuery.of(context).size.width,
-              height: 200,
-              child: Column(
-                children: [
-                  MentionableTextField(
-                    onControllerReady: (value) =>
-                        _onSelectMention = value.pickMentionable,
-                    maxLines: 1,
-                    onSubmitted: print,
-                    mentionables: const [
-                      MyUser('John Doe'),
-                      MyUser('Jeanne Aulas'),
-                      MyUser('Alexandre Dumas')
-                    ],
-                    onMentionablesChanged: (mentionableUsers) =>
-                        setState(() => _mentionableUsers = mentionableUsers),
-                    decoration:
-                        const InputDecoration(hintText: 'Type something ...'),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        final mentionable = _mentionableUsers[index];
-                        return ListTile(
-                          leading: const Icon(Icons.person),
-                          title: Text(mentionable.mentionLabel),
-                          onTap: () => _onSelectMention(mentionable),
-                        );
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xffC0E6EE), Colors.white],
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              children: [
+                Material(
+                  borderRadius: BorderRadius.circular(13),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: MentionableTextField(
+                      onControllerReady: (value) =>
+                          _onSelectMention = value.pickMentionable,
+                      maxLines: 1,
+                      onSubmitted: print,
+                      mentionables: const [
+                        MyUser('John Doe'),
+                        MyUser('Jeanne Aulas'),
+                        MyUser('Alexandre Dumas')
+                      ],
+                      onMentionablesChanged: (mentionables) {
+                        final previousValue =
+                            _mentionableStreamController.value;
+                        _mentionableStreamController.add(mentionables);
+                        if (!listEquals(previousValue, mentionables)) {
+                          _animationController
+                            ..duration = Duration(
+                                milliseconds: 250 * mentionables.length)
+                            ..reset()
+                            ..forward();
+                        }
                       },
-                      itemCount: _mentionableUsers.length,
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.alternate_email),
+                        hintText: 'Type @ to trigger mention',
+                        border: InputBorder.none,
+                      ),
                     ),
-                  )
-                ],
-              ),
-            )
-          ],
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: StreamBuilder<List<Mentionable>>(
+                        stream: _mentionableStreamController.stream,
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) return const SizedBox();
+                          final mentionables = snapshot.data!;
+                          final length = mentionables.length;
+                          return ListView.separated(
+                            itemBuilder: (context, index) {
+                              final mentionable = mentionables[index];
+                              return ScaleTransition(
+                                scale: _animationController
+                                    .drive(
+                                      CurveTween(
+                                          curve: Interval(
+                                        index / length,
+                                        (index + 1) / length,
+                                        curve: Curves.easeIn,
+                                      )),
+                                    )
+                                    .driveBouncy(),
+                                child: Material(
+                                  borderRadius: BorderRadius.circular(13),
+                                  child: ListTile(
+                                    dense: true,
+                                    leading: const Icon(Icons.person),
+                                    title: Text(mentionable.mentionLabel),
+                                    onTap: () => _onSelectMention(mentionable),
+                                    tileColor: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(13)),
+                                  ),
+                                ),
+                              );
+                            },
+                            itemCount: length,
+                            separatorBuilder: (BuildContext context,
+                                    int index) =>
+                                const Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 4)),
+                          );
+                        }),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
